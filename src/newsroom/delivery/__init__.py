a9a0cfa4df304955b8130e1926cca326d28a8288
Mohammad Saleh Mahdinejad <msmahdinejad@gmail.com>
2026-07-13 17:37:15 +0330

    feat: add __init__ for editorial and delivery modules

---
+++ src/newsroom/delivery/__init__.py
@@ -0,0 +1,5 @@
+"""Delivery integrations."""
+
+from newsroom.delivery.telegram import TelegramDelivery
+
+__all__ = ["TelegramDelivery"]
---
+++ src/newsroom/editorial/__init__.py
@@ -0,0 +1,5 @@
+"""Editorial digest generation."""
+
+from newsroom.editorial.hermes import HermesEditorial, create_digest
+
+__all__ = ["HermesEditorial", "create_digest"]
